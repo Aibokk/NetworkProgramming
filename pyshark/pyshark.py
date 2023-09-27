fdff238d7744a1925c7f8bd6e10810dc0bc13fff
Aibokk <tam50@daum.net>
2023-09-27 17:14:14 +0900

    01

--- pyshark/pyshark.py
+++ pyshark/pyshark.py
@@ -4,4 +4,4 @@ from scapy.all import *
 print(conf.ifaces)
 
 while True:
-    sniff(iface="Software Loopback Interface 1" ,prn = lambda x:x.show())
+    sniff(iface="Software Loopback Interface 1", prn = lambda x:x.show())
